ba23e5e06d132b9cf84169bbe71ec915b7ac2073
AmanJit <AmanJit>
2017-03-13 13:26:52 +0530

    Stimulus

--- js/custom.js
+++ js/custom.js
@@ -44,7 +44,6 @@
     $('#about').parallax("100%", 0.3);
     $('#service').parallax("100%", 0.2);
     $('#experience').parallax("100%", 0.3);
-    $('#education').parallax("100%", 0.1);
     $('#quotes').parallax("100%", 0.3);
     $('#contact').parallax("100%", 0.1);
     $('footer').parallax("100%", 0.2);
