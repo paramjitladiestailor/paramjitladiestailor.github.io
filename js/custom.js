b57d85d7db35d797b68625eb818caa55b3920bee
AmanJit <AmanJit>
2017-03-13 13:03:38 +0530

    Stimulus

--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,65 @@
 
- /* jQuery Pre loader
-  -----------------------------------------------*/
-$(window).load(function(){
+
+  /*-------------------------------------------------------------------------------
+    PRE LOADER
+  -------------------------------------------------------------------------------*/
+
+  $(window).load(function(){
     $('.preloader').fadeOut(1000); // set duration in brackets    
-});
+  });
 
 
-$(document).ready(function() {
 
-  /* Hide mobile menu after clicking on a link
-    -----------------------------------------------*/
+  /* HTML document is loaded. DOM is ready. 
+  -------------------------------------------*/
+
+  $(document).ready(function() {
+
+
+  /*-------------------------------------------------------------------------------
+    Navigation - Hide mobile menu after clicking on a link
+  -------------------------------------------------------------------------------*/
+
     $('.navbar-collapse a').click(function(){
         $(".navbar-collapse").collapse('hide');
     });
 
 
-  /* Smoothscroll js
-  -----------------------------------------------*/
+    $(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+  });
+
+
+
+  /*-------------------------------------------------------------------------------
+    jQuery Parallax
+  -------------------------------------------------------------------------------*/
+
+    function initParallax() {
+    $('#home').parallax("100%", 0.1);
+    $('#about').parallax("100%", 0.3);
+    $('#service').parallax("100%", 0.2);
+    $('#experience').parallax("100%", 0.3);
+    $('#education').parallax("100%", 0.1);
+    $('#quotes').parallax("100%", 0.3);
+    $('#contact').parallax("100%", 0.1);
+    $('footer').parallax("100%", 0.2);
+
+  }
+  initParallax();
+
+
+
+  /*-------------------------------------------------------------------------------
+    smoothScroll js
+  -------------------------------------------------------------------------------*/
+  
     $(function() {
-        $('.navbar-default a').bind('click', function(event) {
+        $('.custom-navbar a, #home a').bind('click', function(event) {
             var $anchor = $(this);
             $('html, body').stop().animate({
                 scrollTop: $($anchor.attr('href')).offset().top - 49
@@ -26,59 +67,15 @@ $(document).ready(function() {
             event.preventDefault();
         });
     });
+  
 
 
- /* Home Slideshow Vegas
-  -----------------------------------------------*/
-  $(function() {
-    $('body').vegas({
-        slides: [
-            { src: 'images/slide-img1.jpg' },
-            { src: 'images/slide-img2.jpg' },
-            { src: 'images/slide-img3.jpg' }
-        ],
-        timer: false,
-        transition: [ 'zoomIn', ],
-        animation: ['kenburns']
-    });
-  });
+  /*-------------------------------------------------------------------------------
+    wow js - Animation js
+  -------------------------------------------------------------------------------*/
 
-
-  /* Team carousel
-  -----------------------------------------------*/
-  $(document).ready(function() {
-      $("#team-carousel").owlCarousel({
-          items : 3,
-          itemsDesktop : [1199,3],
-          itemsDesktopSmall : [979,3],
-          slideSpeed: 300,
-          itemsDesktop : [1199,2],
-          itemsTablet: [768,1],
-          itemsTabletSmall: [985,2],
-          itemsMobile : [479,1],
-      });
-    });
-    
-
-    /* Back to Top
-    -----------------------------------------------*/
-    $(window).scroll(function() {
-      if ($(this).scrollTop() > 200) {
-          $('.go-top').fadeIn(200);
-            } else {
-                $('.go-top').fadeOut(200);
-           }
-        });   
-          // Animate the scroll to top
-        $('.go-top').click(function(event) {
-          event.preventDefault();
-        $('html, body').animate({scrollTop: 0}, 300);
-    });
-
-
-  /* wow
-  -------------------------------*/
   new WOW({ mobile: false }).init();
+
 
   });
 
